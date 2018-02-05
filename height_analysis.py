d242faaeae0eaadcaca136b0a8e5a60b5d9d49cc
alone <rahulrajias@gmail.com>
2018-02-05 19:47:04 +0530

    Analysis on single plot

--- height_analysis.py
+++ height_analysis.py
@@ -11,19 +11,17 @@ pop_std=np.std(plot3)
 # print(type(pop_std))
 print("pop_avg: %s and pop_std: %s",(pop_avg, pop_std))
 # print("pop_std: ",pop_std)
-sample=5
+sample=19
 sampled_data= list(itertools.combinations(plot3,sample))
 # print(sampled_data)
 avg_list=[]
 for i in sampled_data:
     avg_list.append(np.average(i))
 print(avg_list)
-plt.hist(avg_list,bins=50, color='r', histtype="step")
-plt.show()
+# plt.hist(avg_list,bins=50, color='r', histtype="step")
+# plt.show()
 
-# sample_overall_avg=np.average(avg_list)
-# print("Overall average: ",sample_overall_avg)
-# SD=np.std(avg_list)
-# print("Std of the avg_samples: ",SD)
-
-# ax.hist(data,bins=100,range=(minimum,maximum),facecolor="r", histtype = 'step')
+sample_overall_var=np.var(avg_list)
+print("Overall variance: ",sample_overall_var)
+SD=np.std(avg_list)
+print("Std of the avg_samples: ",SD)
